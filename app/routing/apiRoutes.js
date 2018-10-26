c282b9c9d44fc807bce208204680506d426f48c6
Jack Morrissey <j.morrissey.16@gmail.com>
2018-10-26 02:02:15 -0500

    updated matching logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,11 +6,26 @@ module.exports = function(app) {
     });
 
     app.post("/api/friends", (req, res) => {
-        let newFriend = req.body;
-        
+        var newFriend = req.body;
+        var totalDiff = 100;
+
+        for (let i in friendsArrayOfObjects) {
+            var diff = 0
+            for (let k in friendsArrayOfObjects[i].scores) {
+                var checked = newFriend[i].scores[k];
+                var current = req.body.scores[k]
+                diff += Math.abs(parseInt(checked) - parseInt(current));
+            }
+            if (diff < totalDiff) {
+                totalDiff = diff;
+                var matchToSend = {
+                    name: friendsArrayOfObjects[i].name,
+                    picture: friendsArrayOfObjects[i].picture
+                }
+            };
+        };
         console.log(newFriend);
-        // *PUSH THE newFriend INTO THE FRIENDS ARRAY* // 
-        
-        res.json(newFriend);
-    })
+        friendsArrayOfObjects.push(newFriend);
+        res.send(matchToSend);  
+    });
 }
