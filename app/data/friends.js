cb76f99115feda6c1aa7adc05513356c5bc66143
Jack Morrissey <j.morrissey.16@gmail.com>
2018-10-26 01:21:35 -0500

    updated friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,30 @@
 
-let friendsArrayOfObjects = []
+let friendsArrayOfObjects = [
+    {
+        name: Maeve,
+        picture: "https://vignette.wikia.nocookie.net/westworld/images/d/d8/Maeve_Les_Ecorches.jpg/revision/latest?cb=20180530183051",
+        scores: [1, 4, 5, 3, 2, 1, 4, 3, 1, 1]
+    }, 
+    {
+        name: Bernard,
+        picture: "https://vignette.wikia.nocookie.net/westworld/images/b/b6/Bernard_Les_Ecorches.png/revision/latest/scale-to-width-down/620?cb=20180604190617",
+        scores: [5, 4, 1, 2, 4, 3, 1, 5, 2, 3]
+    }, 
+    {
+        name: Ford,
+        picture: "https://vignette.wikia.nocookie.net/westworld/images/2/20/Robert_Ford_Les_Echorces.jpg/revision/latest?cb=20180815194324",
+        scores: [1, 5, 2, 3, 1, 4, 2, 1, 5, 5]
+    }, 
+    {
+        name: Dolores,
+        picture: "https://vignette.wikia.nocookie.net/westworld/images/5/51/Dolores_Abernathy_Vanishing_Point.jpg/revision/latest?cb=20180613181613",
+        scores: [1, 2, 1, 1, 4, 1, 3, 2, 1, 2]
+    }, 
+    {
+        name: Teddy,
+        picture: "https://vignette.wikia.nocookie.net/westworld/images/a/a9/Teddy_Flood_Phase_Space.jpg/revision/latest/scale-to-width-down/620?cb=20180526115338",
+        scores: [3, 3, 2, 4, 3, 2, 5, 2, 1, 3]
+    }]
 
 
 module.exports = friendsArrayOfObjects
